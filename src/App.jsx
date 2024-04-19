dec54d0a2b064845a68b1a40f447cbee4faa41de
avayyyyyyy <jabhi465@gmail.com>
2024-04-19 17:51:52 +0530

    temp

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [data, setData] = useState();
+
+    const fetchData = async () => {
+        const res = await fetch("https://randomuser.me/api/");
+        const jsonData = await res.json();
+        setData(jsonData);
+    };
+
+    fetchData();
+
+  return (
+    <div>
+        <div>
+          <h1>User Data</h1>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    </div>
+  );
+};
+
+export default App;
